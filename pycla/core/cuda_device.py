50deaecd2f3f78e88d9547805ff083307633e224
Moésio Filho <moesio.f@gmail.com>
2025-04-15 11:37:37 -0300

    feat: add CUDADevices for pycla

--- pycla/core/cuda_device.py
+++ pycla/core/cuda_device.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+from pycla.bin import CLA
+
+
 @dataclass(frozen=True)
 class CUDADevice:
     id: int
@@ -10,8 +13,56 @@ class CUDADevice:
 
 
 class Devices:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
     def __init__(self):
-        pass
+        # Initialize devices
+        self._devices = []
+        for i in range(CLA.cuda_device_count):
+            dev = CLA.get_device_by_id(i).contents
+            self._devices.append(
+                CUDADevice(
+                    id=dev.id,
+                    name=dev.name.decode("utf-8"),
+                    max_grid=(
+                        dev.max_grid_size_x,
+                        dev.max_grid_size_y,
+                        dev.max_grid_size_z,
+                    ),
+                    max_block=(
+                        dev.max_block_size_x,
+                        dev.max_block_size_y,
+                        dev.max_block_size_z,
+                    ),
+                    max_threads_per_block=dev.max_threads_per_block,
+                )
+            )
 
+    def __len__(self) -> int:
+        return len(self._devices)
 
-DEVICES: Devices = Devices()
+    def __getitem__(self, key: int | str) -> CUDADevice:
+        is_int = isinstance(key, int)
+        if not (is_int or isinstance(key, str)):
+            raise TypeError("Key must be either int or string.")
+
+        if is_int and (key < 0 or key > len(self)):
+            raise IndexError("Integer key is out of bounds.")
+
+        if is_int:
+            return self._devices[key]
+
+        # Try to find device with respective name
+        device = next((d for d in self._devices if d.name == key), None)
+        if device is None:
+            raise KeyError("No device with this name found.")
+        return device
+
+    def __str__(self) -> str:
+        return f"[{','.join(self._devices)}]"
