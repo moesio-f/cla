9e281dc3667e20a3e070d251b1eea3164f4116d4
Moésio Filho <moesio.f@gmail.com>
2025-04-23 10:24:40 -0300

    feat: update kernel launch parameters in CLA
    - Add support for launching with device params if they are compatible
      with problem and device;
    - Add warning when the parameters are not compatible (try with fallback
      instead of raising error);
    - Update version to 1.1.0 in cla/CMakeLists.txt;
    - Update pycla.bin.cla with new struct for Kernel Launch params;
    - Update version in pyproject.toml to 1.1.0;

--- cla/cuda/utils.cu
+++ cla/cuda/utils.cu
@@ -6,14 +6,20 @@ extern "C" {
 #include "../include/vector_utils.h"
 #include <assert.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 }
 
+#define RED "\x1B[31m"
+#define RESET "\x1B[0m"
 #include "cuda_runtime_api.h"
 
 typedef struct {
   dim3 n_threads;
   dim3 n_blocks;
 } KernelLaunchParams;
+
+typedef enum { PtC_X, PtC_Y, PtC_Z } ParamsToCheck;
 
 int _find_n(int dims, int n, int coef) {
   // Simple algorithm to find an appropriate
@@ -27,40 +33,132 @@ int _find_n(int dims, int n, int coef) {
   return coef * k;
 }
 
+bool _are_params_set(CUDAKernelLaunchParameters params, ParamsToCheck check) {
+  switch (check) {
+  case PtC_X:
+    return params.n_threads_x > 0 && params.n_blocks_x > 0;
+  case PtC_Y:
+    return params.n_threads_y > 0 && params.n_blocks_y > 0;
+  case PtC_Z:
+    return params.n_threads_z > 0 && params.n_blocks_z > 0;
+  }
+
+  return false;
+}
+
+bool _are_params_compatible(CUDADevice *device, int dims, ParamsToCheck check) {
+  // Obtain n_threads, n_blocks, max_threads, max_blocks
+  //    for the selected dimension
+  int nt = 0, nb = 0, mt = 0, mb = 0;
+  switch (check) {
+  case PtC_X:
+    nt = device->params.n_threads_x;
+    nb = device->params.n_blocks_x;
+    mt = device->max_block_size_x;
+    mb = device->max_grid_size_x;
+    break;
+  case PtC_Y:
+    nt = device->params.n_threads_y;
+    nb = device->params.n_blocks_y;
+    mt = device->max_block_size_y;
+    mb = device->max_grid_size_y;
+    break;
+  case PtC_Z:
+    nt = device->params.n_threads_z;
+    nb = device->params.n_blocks_z;
+    mt = device->max_block_size_z;
+    mb = device->max_grid_size_z;
+    break;
+  }
+
+  // Conditions to fit problem in GPU
+  return (nt * nb >= dims) && (nt <= mt) && (nb <= mb);
+}
+
 KernelLaunchParams get_vector_launch_parametes(CUDADevice *device,
                                                int vec_dims) {
-  int max_threads = device->max_threads_per_block;
-  int max_grid = device->max_grid_size_x;
-  int n = _find_n(vec_dims, 2, KERNEL_LAUNCH_COEF);
+  int n_threads = 0, n_blocks = 0;
+
+  // Check if device has compatible parameters
+  if (_are_params_set(device->params, PtC_X)) {
+    if (_are_params_compatible(device, vec_dims, PtC_X)) {
+      n_threads = device->params.n_threads_x;
+      n_blocks = device->params.n_blocks_x;
+    } else {
+      // Warn user about fallback
+      printf(
+          RED
+          "[CLA] Device (id=%d, name='%s') has parameters set, but they "
+          "are not compatible with either GPU/Problem. Using fallback." RESET,
+          device->id, device->name);
+    }
+  }
+
+  // Fallback algorithm
+  if (n_threads <= 0) {
+    n_threads = _find_n(vec_dims, 2, KERNEL_LAUNCH_COEF);
+    n_blocks = n_threads;
+  }
 
   // Assert we have enough threads/blocks to compute
   //    and that those values fit the device capabilities
-  assert(n * n >= vec_dims);
-  assert(n <= max_threads);
-  assert(n <= max_grid);
+  int max_threads = device->max_block_size_x;
+  int max_grid = device->max_grid_size_x;
+  assert(n_threads * n_blocks >= vec_dims);
+  assert(n_threads <= max_threads);
+  assert(n_blocks <= max_grid);
 
-  return {dim3(n), dim3(n)};
+  // Return parameters
+  return {dim3(n_threads), dim3(n_blocks)};
 }
 
 KernelLaunchParams get_matrix_launch_parametes(CUDADevice *device, int mat_rows,
                                                int mat_columns) {
-  int max_threads = device->max_threads_per_block;
-  int max_grid_x = device->max_grid_size_x;
-  int max_grid_y = device->max_grid_size_y;
-  int max_threads_dim = (int)floor(sqrt(max_threads));
-  int n_x = _find_n(mat_rows, 2, KERNEL_LAUNCH_COEF);
-  int n_y = _find_n(mat_columns, 2, KERNEL_LAUNCH_COEF);
+  int n_threads_x = 0, n_blocks_x = 0, n_threads_y = 0, n_blocks_y = 0;
+
+  // Check if device has compatible parameters
+  if (_are_params_set(device->params, PtC_X) &&
+      _are_params_set(device->params, PtC_Y)) {
+    if (_are_params_compatible(device, mat_rows, PtC_X) &&
+        _are_params_compatible(device, mat_columns, PtC_Y)) {
+      n_threads_x = device->params.n_threads_x;
+      n_blocks_x = device->params.n_blocks_x;
+      n_threads_y = device->params.n_threads_y;
+      n_blocks_y = device->params.n_blocks_y;
+    } else {
+      // Warn user about fallback
+      printf(
+          RED
+          "[CLA] Device (id=%d, name='%s') has parameters set, but they "
+          "are not compatible with either GPU/Problem. Using fallback." RESET,
+          device->id, device->name);
+    }
+  }
+
+  // Fallback algorithm
+  if (n_threads_x <= 0) {
+    n_threads_x = _find_n(mat_rows, 2, KERNEL_LAUNCH_COEF);
+    n_threads_y = _find_n(mat_columns, 2, KERNEL_LAUNCH_COEF);
+    n_blocks_x = n_threads_x;
+    n_blocks_y = n_threads_y;
+  }
 
   // Assert we have enough threads/blocks to compute
   //    and that those values fit the device capabilities
-  assert(n_x * n_x >= mat_rows);
-  assert(n_y * n_y >= mat_columns);
-  assert(n_x <= max_threads_dim);
-  assert(n_y <= max_threads_dim);
-  assert(n_x <= max_grid_x);
-  assert(n_y <= max_grid_y);
+  int max_threads_x = device->max_block_size_x;
+  int max_threads_y = device->max_grid_size_y;
+  int max_threads = device->max_threads_per_block;
+  int max_grid_x = device->max_grid_size_x;
+  int max_grid_y = device->max_grid_size_y;
+  assert(n_threads_x * n_blocks_x >= mat_rows);
+  assert(n_threads_y * n_blocks_y >= mat_columns);
+  assert(n_threads_x * n_threads_y <= max_threads);
+  assert(n_threads_x <= max_threads_x);
+  assert(n_threads_y <= max_threads_y);
+  assert(n_blocks_x <= max_grid_x);
+  assert(n_blocks_y <= max_grid_y);
 
-  return {dim3(n_x, n_y), dim3(n_x, n_y)};
+  return {dim3(n_threads_x, n_threads_y), dim3(n_blocks_x, n_blocks_y)};
 }
 
 extern "C" Vector *cpu_gpu_conditional_apply_vector_operator(
